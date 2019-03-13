384cf12f51b15e758b5c6077f75ea5aae2cb5aff
Macr0Nerd <macronerdart@gmail.com>
2019-03-13 07:30:33 -0500

    Moved pluginsInfo one directory up and verified it works with btc plugin

---
+++ honeybot/pluginInfo.py
@@ -0,0 +1,56 @@
+"""
+[pluginInfo.py]
+Plugin Info Plugin
+
+[Author]
+Gabriele Ron
+
+[About]
+Returns info about each plugin
+
+[Commands]
+>>> ...
+returns the plugins info
+"""
+
+import os
+import sys
+from importlib import import_module
+import importlib.util
+
+directory = os.fsencode(__file__[:-13] + "/plugins")
+
+def docFind(lines, att):
+    hit = False
+
+    for line in lines.splitlines():
+
+        if hit:
+            auth = str(line)
+            auth = auth.strip()
+            return auth
+
+        if line == f"[{att}]":
+            hit = True
+
+    return f"Unknown {att}"
+
+
+f = open(__file__[:-13] + "/plugins_info.md", "w")
+plug = ""
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+
+    if filename.endswith(".py"):
+        spec = importlib.util.spec_from_file_location(filename[:-3], __file__[:-13] + "plugins/" + filename)
+        x = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(x)
+
+        if isinstance(x.__doc__, str):
+            plug += filename + "\\" + "\n"
+            plug += "by " + docFind(x.__doc__, "Author") + ", " + docFind(x.__doc__, "Website") + "\\" + "\n"
+            plug += docFind(x.__doc__, "About") + "\n" + "\n"
+
+f.write(plug)
+f.close()
