b44a26e92f2da59405618b3afdd41282766142ce
“[Justin <jrwalker314@yahoo.com>
2019-04-03 19:13:42 -0700

    Create structure for hangman class.

--- honeybot/plugins/hangman.py
+++ honeybot/plugins/hangman.py
@@ -16,16 +16,38 @@ class Plugin:
     def __init__(self):
         pass
 
-    def __hangman(word):
-        if word.lowercase() == "start" 
+    def __hangman(self, command):
+        pass
 
     def run(self, incoming, methods, info):
             try:
                 msgs = info['args'][1:][0].split()
                 if info['command'] == 'PRIVMSG':
                     if len(msgs) > 1:
-                        if msgs[0] == '.dictionary':
+                        if msgs[0] == '.hangman':
                             word = msgs[1]
-                            methods['send'](info['address'], Plugin.__dictionary(self, word))
-            excpet Exception as e:
+                            methods['send'](info['address'], Plugin.__hangman(self, command))
+            except Exception as e:
                 print('woops plugin error', e)
+
+class Hangman:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def guess_letter(self, guessLetter):
+        pass
+
+    def guess_word(self, wordGuess):
+        pass
+
+    def check_win(self):
+        pass
+
+    def decrement_guesses(self):
+        pass
+
+    def display_screen(self):
+        pass
