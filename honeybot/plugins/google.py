ec6e19a04d931f0df1e4df61ac593857ca853d10
“[Justin <jrwalker314@yahoo.com>
2019-04-08 16:03:28 -0700

    Make google working. Need to refactor for longer search term.

--- honeybot/plugins/google.py
+++ honeybot/plugins/google.py
@@ -10,7 +10,7 @@ Justin Walker
 Returns the first several links from a google search.
 
 [Commands]
->>> .google <<search term>> <<optional link count, default 10>>
+>>> .google <<search term>> <<optional link count, default 3>>
 returns search links
 """
 
@@ -24,11 +24,11 @@ class Plugin:
     def __init__(self):
         pass
 
-    def __google(self, search_term, search_count):
+    def __google(search_term, search_count):
         # num is number of links, start is what link to start with,
         # only_standard limits it to normal links instead of adds and extra
         # links.
-        return search(search_term, num=search_count, start=0,
+        return search(search_term, start=1, stop=search_count, \
                       only_standard=True)
 
     def run(self, incoming, methods, info):
@@ -36,19 +36,38 @@ class Plugin:
             msgs = info['args'][1:][0].split()
    
             if info['command'] == 'PRIVMSG' and msgs[0] == '.google':
-                count = 10
                 # First argurment after .google should be search term,
                 # if it exists
-                if len(msgs) == 2:
-                    term = msgs[1]
-                    # Second argument should be count if it exists.
-                    if len(msgs) == 3:
-                        count = msgs[2]
-
-                    for link in self.__google(term, count):
-                        methods['send'](info['address'], link)
+                count = 3
+                term = ''
+                if len(msgs) > 1:
+                    for msg in msgs[1:]:
+                        if msg[0] == "'":
+                            #Find a way to separate int count from whole string mesage.
+                        for link in Plugin.__google(term, count):
+                            methods['send'](info['address'], link)
                 else:
                     methods['send'](info['address'], "Input error. '.google search_term search_count'.")
     
         except Exception as e:
             print('woops plugin error: ', e)
+
+
+
+def send(info, message):
+    print(message)
+
+
+def test_them(plugin, msg):
+
+    methods = {"send":send}
+    msg = msg
+    info = {'args':[None,msg],
+            'command':'PRIVMSG',
+            'address':'That place'}
+    plug.run("",methods,info)
+
+
+plug = Plugin()
+
+test_them(plug, ".google something")
