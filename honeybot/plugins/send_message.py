3cb4d7fbb16367e424c75d6a76d6177648e7856c
“[Justin <jrwalker314@yahoo.com>
2019-04-09 23:12:17 -0700

    Clean for PEP8, missing online check.

--- honeybot/plugins/send_message.py
+++ honeybot/plugins/send_message.py
@@ -11,7 +11,13 @@ responds to .hi, demo of a basic plugin
 
 [Commands]
 >>> .send #channel .u riceabove .m how are you?
+Sends a message to #channel like
+'IronPenguin from #channel says to riceabove: how are you?'
+
 >>> .send #channel hi all
+Sends a message to #channel like
+'IronPenguin from #channel says: hi all'
+
 Checks if user online first and says "User not online."
 """
 
@@ -22,51 +28,35 @@ class Plugin:
 
     def run(self, incoming, methods, info):
         try:
-            print(info)
             msgs = info['args'][1:][0].split()
             if info['command'] == 'PRIVMSG' and msgs[0] == '.send':
                 if len(msgs) >= 3:
+                    bang = info['prefix'].find('!')
+                    message = ''
 
                     channel_destination = msgs[1]
-                    bang = info['prefix'].find('!')
                     sender = info['prefix'][0:bang]
                     sender_channel = '#'+info['address']
-                    message = ''
 
                     if msgs[2][0] != '.':
                         for word in msgs[2:]:
-                            message += word
-                        complete_message = sender+' from '+sender_channel+' says: '+message
-                        methods['send'](channel_destination, complete_message)
-                    elif len(msgs) >= 6 and msgs[2] == '.u' and msgs[4] == '.m':
+                            message += word + ' '
+                        complete_message = ('{0} from {1} says: '
+                                            .format(sender,sender_channel,
+                                            message.rstrip()))
+
+                    elif (len(msgs) >= 6 and 
+                          msgs[2] == '.u' and
+                          msgs[4] == '.m'):
                         user_recipient = msgs[3]
                         for word in msgs[5:]:
-                            message += word
-                        complete_message = sender+' from '+sender_channel+' says to '+user_recipient+': '+message
-                        methods['send'](channel_destination, complete_message)
-
+                            message += word + ' '
+                        complete_message = ('{0} from {1} says to {2}: {3}'
+                                           .format(sender,sender_channel,
+                                           user_recipient,message.rstrip()))
+                    
+                    methods['send'](channel_destination, complete_message)
                 else:
                     methods['send'](info['address'], 'Command input error.')
         except Exception as e:
             print('woops plugin error: ', e)
-
-
-def send(info, message):
-    print(message)
-
-
-def test_them(plugin, msg):
-
-    methods = {"send":send,
-               "send_raw":send}
-    msg = msg
-    info = {'args':[None,msg],
-            'command':'PRIVMSG',
-            'address':'That place',
-            'prefix':'IronPenguin!4b717577@gateway/web/cgi-irc/kiwiirc.com/ip.75.113.117.119'}
-    plug.run("",methods,info)
-
-
-plug = Plugin()
-
-test_them(plug, ".send #start .u IronPenguin .m Aurene Lives")
