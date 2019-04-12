37fb91d1c4c7105989066c0797c9714ffdf6ef0a
Macr0Nerd <macronerdart@gmail.com>
2019-04-12 18:19:05 -0500

    The base for the HoneyGUI

--- workshop/HoneyGUI/main.cpp
+++ workshop/HoneyGUI/main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
+#include "guiBase.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+//To manually build, run 'g++ -o test main.cpp guiBase.cpp  optionDisplay.cpp optionList.cpp $(pkg-config gtkmm-3.0 --cflags --libs)'
+
+int main(int argc, char *argv[]) {
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example"); //Simply creates the application window to run
+    //everything in
+
+    guiBase window; //Creates the window for all the widgets based on guiBase
+
+    return app->run(window); //Starts the runtime loop for GTK
 }
