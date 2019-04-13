00cb98fbaa96c7a2493668c875e8d3a8593d934d
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-04-13 12:01:52 +0100

    Update news.py

--- honeybot/plugins/news.py
+++ honeybot/plugins/news.py
@@ -6,8 +6,8 @@ Angelo Giacco
 [About]
 Gets the top 10 headlines around the world from bbc world news
 [Commands]
->>> .hi
-returns hoo
+>>> .news
+returns string of ten news headlines with newlines between
 """
 from bs4 import BeautifulSoup
 import requests
