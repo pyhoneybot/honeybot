55ca4e6638eb1ed0976702550f8e6e2541aaba6f
Gico Carlo Evangelista <gnevange@ucsc.edu>
2019-04-14 01:36:44 -0700

    added time

--- honeybot/plugins/log.py
+++ honeybot/plugins/log.py
@@ -15,7 +15,7 @@ Logs the chat into a log.txt file
 # standard python file handling
 
 import os
-from datetime import date
+from datetime import datetime
 
 # Makes a new log dir if not existent
 # Change according to where you have honeybot dir
@@ -23,11 +23,8 @@ log_path = "{}/log".format(os.getcwd())
 if not os.path.exists(log_path):
     os.makedirs('log')
 
-# curr date
-curr = date.today().strftime("%d-%m-%Y")
-
-# Make log file (if not existent) using current date
-f = open('log/{}.txt'.format(curr), 'a+')
+# Current date
+curr_date = datetime.today().strftime("%d-%m-%Y")
 
 class Plugin:
 
@@ -38,13 +35,19 @@ class Plugin:
         msgs = info['args'][1:]
         try:
             if info['command'] == 'PRIVMSG':
+                # Make log file (if not existent) using current date
+                f = open('log/{}.txt'.format(curr_date), 'a+')
+
+                # Current time
+                curr_time = datetime.now().strftime('%H:%M:%S')
+
                 # Parse the user ID from info['prefix']
                 raw_user = info['prefix']
                 user_index = raw_user.find('!')
                 user = raw_user[0:user_index]
 
                 # Logging the chat into txt file
-                f.write('{}: {}\r\n'.format(user, msgs[0]))
+                f.write('{}: {}: {}\r\n'.format(curr_time, user, msgs[0]))
 
         except Exception as e:
             print('woops plugin error ', e)
