09a675e53bba95d10a01e240128d2dc9d670faa1
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-04-14 10:44:10 +0100

    syncro

--- honeybot/lab.py
+++ honeybot/lab.py
@@ -1,15 +1,1 @@
-import importlib
-
-reqs = []
-with open('../requirements.txt') as f:
-    reqs = f.read().split('\n')
-
-
-reqs = [m.split('==')[0] for m in reqs if m]
-
-for module in reqs:
-    try:
-        importlib.import_module(module)
-    except ModuleNotFoundError as e:
-        print('not found:', module)
-print(reqs)
+print(1)
