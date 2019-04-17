cac45eabf7a42bcffee4ebfefcfabbe4949117b8
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-04-17 11:44:18 +0100

    kick command testing

--- honeybot/plugins/russian_roulette.py
+++ honeybot/plugins/russian_roulette.py
@@ -29,7 +29,7 @@ class Plugin:
         if kill:
             name = info["prefix"].split("!")[0]
             channel = info["address"]
-            kill_command = "/kick "+ channel + " " + name
+            kill_command = "/KICK "+ channel + " " + name + " \r\n"
             methods["send"](info["address"],kill_command)
             #code to quit the channel
             return "Suicide is always a risk when playing russian roulette... RIP..."
