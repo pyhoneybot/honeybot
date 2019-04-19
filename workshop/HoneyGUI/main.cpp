7692f66671b0bea6e073ecfd8d7d84064f9c0cd3
Macr0Nerd <macronerdart@gmail.com>
2019-04-19 17:44:24 -0500

    Coming up to date

--- workshop/HoneyGUI/main.cpp
+++ workshop/HoneyGUI/main.cpp
@@ -1,6 +1,15 @@
+#include "guiBase.h"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+//To manually build, run 'g++ -o test main.cpp guiBase.cpp controlDisplay.cpp configDisplay.cpp pluginDisplay.cpp $(pkg-config gtkmm-3.0 --cflags --libs)'
+
+int main(int argc, char *argv[]) {
+    system("x=$(find ~/ -name \"honeybot\" | head -n 1); echo $x >> path.txt");
+
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example"); //Simply creates the application window to run
+    //everything in
+
+    guiBase window;
+
+    return app->run(window); //Starts the runtime loop for GTK
 }
