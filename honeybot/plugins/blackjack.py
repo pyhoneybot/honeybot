55ebf30de9bf040fde6f46bc89f5db2ea3bc7663
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-05-28 17:30:27 +0100

    almost working

--- honeybot/plugins/blackjack.py
+++ honeybot/plugins/blackjack.py
@@ -13,7 +13,7 @@ creates blackjack game
 >>>.blackjack join
 join blackjack game
 
->>>.blackjack round
+>>>.blackjack start
 start a round
 
 >>>.blackjack hit
@@ -21,10 +21,8 @@ get another card
 
 >>>.blackjack stand
 move on to next player
-
->>>.blackjack leave
-leave the game
 """
+
 import random,sys,os
 sys.path.append('plugins/poker_assets')
 import deck
@@ -36,11 +34,11 @@ class Plugin():
 
     bj_created = False
     round_started = False
-    dec_req = False
     turn = 0
     player_lst = []
     starting_chips = 100
     winner = None
+    end = False
 
 
     def __init__(self):
@@ -51,44 +49,64 @@ class Plugin():
     """
 
     def next_turn():
-        '''increment turn and set to start if will cause index error'''
+        ''' increment turn and set to start if will cause index error '''
+
         try:
             Plugin.turn += 1
             if Plugin.turn == len(Plugin.player_lst):
                 Plugin.turn -= len(Plugin.player_lst)
+                #find winner
+                index,value = max([player.show_player_hand().hand_total() for player in Plugin.player_lst])
+                Plugin.winner = Plugin.player_lst[index].get_name()
+                Plugin.bj_created = False
+                methods["send"](info["address"],"The winner is "+winner+"!")
         except Exception as e:
             print("woops, poker turn change error ",e)
 
     def initPlayer(methods,info):
-        '''add a player to the round'''
+        ''' add a player to the round '''
 
-        name = info["prefix"].split("!")[0]
-        if len(Plugin.player_lst) <= 5: #limit game to 6 players
-            if not(name in Plugin.player_lst):
-                Plugin.player_lst.append(player.Player(len(Plugin.player_lst),Plugin.starting_chips,name))
-                methods["send"](info["address"],name+" joined the game!")
+        if Plugin.bj_created:
+            if not Plugin.round_started:
+                name = info["prefix"].split("!")[0]
+                if len(Plugin.player_lst) <= 5: #limit game to 6 players
+                    if not(name in [player.get_name() for player in Plugin.player_lst]):
+                        Plugin.player_lst.append(player.Player(len(Plugin.player_lst),Plugin.starting_chips,name))
+                        methods["send"](info["address"],name+" joined the game!")
+                    else:
+                        methods["send"](info["address"],"You are already in this round!")
+                else:
+                    methods["send"](info["address"],"This round is full already")
             else:
-                methods["send"](info["address"],"You are already in this round!")
+                methods["send"](info["address"],"The round has started, wait for a new one to join!")
         else:
-            methods["send"](info["address"],"This round is full already")
+            methods["send"](info["address"],"A game has not yet been created!")
 
     def checkHand(methods,info,player):
-        '''check the value of a player's hand'''
+        ''' check the value of a player's hand '''
 
-
+        total = player.show_player_hand().hand_total()
+        cards = " ".join([card.show_card() for card in player.show_player_hand().show_hand_obj()])
+        if total > 21:
+            methods["send"](info["address"],"Your hand "+cards+" has a value of "+str(total)+" so you have been kicked out")
+        elif total == 21:
+            methods["send"](info["address"],"Your hand "+cards+" has a value of 21 so you have won!")
+        else:
+            methods["send"](info["address"],"Your hand "+cards+" has a value of "+str(total)+".")
 
     """
     COMMAND FUNCTIONS
     """
 
     def initGame(methods,info):
-        '''create a new round'''
+        ''' create a new round '''
 
         if not Plugin.bj_created:
+            Plugin.winner = None
             Plugin.player_lst = []
             name = info["prefix"].split("!")[0]
-            Plugin.initPlayer(methods,info)
             Plugin.bj_created = True
+            Plugin.initPlayer(methods,info)
             Plugin.round_started = False
             Plugin.DECK = deck.Deck()
             methods["send"](info["address"],name+" has started a game of blackjack! Use .blackjack join to join in!")
@@ -96,33 +114,41 @@ class Plugin():
             methods["send"](info["address"],"A game already exists!")
 
     def start(methods,info):
-        '''start the game'''
+        ''' start the game '''
 
         Plugin.round_started = True
         Plugin.bj_created = True
-        Plugin.dec_req = True
         for player in Plugin.player_lst:
             player.add_hand(hand.Hand(Plugin.DECK.make_hand()))
             methods["send"](info["address"],"Showing hand of "+player.get_name())
             total = player.show_player_hand().hand_total()
             cards = " ".join([card.show_card() for card in player.show_player_hand().show_hand_obj()])
-            methods["send"](info["address"],"Here is the hand: "+cards+" which have a total value of "+str(total))
-            if total == 21:
-                methods["send"](info["address"],player.get_name()+" has a hand worth 21 and so has won!")
-                Plugin.winner = player.get_name()
-            #should send each user their hand
+            Plugin.checkHand(methods,info,player)
 
     def hit(methods,info):
+        ''' give player a new card '''
+
         name = info["prefix"].split("!")[0]
-        if Plugin.player_lst[turn].get_name() == name:
+        if Plugin.player_lst[Plugin.turn].get_name() == name:
             if Plugin.winner == None:
-                player_lst[turn].add_card_to_hand(deck.draw_random_card())
-                Plugin.checkHand(methods,info,player_lst[turn])
+                Plugin.player_lst[Plugin.turn].add_card_to_hand(deck.draw_random_card())
+                Plugin.checkHand(methods,info,player_lst[Plugin.turn])
             else:
                 methods["send"](info["address"],"The round is already over and has been won by "+Plugin.winner)
         else:
             methods["send"](info["address"],"It is not your turn!")
 
+    def stand(methods,info):
+        ''' player chooses not to get a new car '''
+        name = info["prefix"].split("!")[0]
+        if Plugin.player_lst[Plugin.turn].get_name() == name:
+            if Plugin.winner == None:
+                methods["send"](info["address"],info["prefix"].split("!")[0]+" has chosen not to pick another card!")
+                Plugin.next_turn()
+            else:
+                methods["send"](info["address"],Plugin.winner+" has already won the game!")
+        else:
+            methods["send"](info["address"],"It is not your turn!")
 
     """
     RUNNING PLUGIN
@@ -131,7 +157,7 @@ class Plugin():
     def run(self, incoming, methods, info, bot_info):
         try:
             msgs = info['args'][1:][0].split()
-            if info['command'] == 'PRIVMSG' and (msgs[0] == ".bj" or msgs[0] == ".21" or msgs[0] == ".blackjack"):
+            if info['command'] == 'PRIVMSG' and (msgs[0] == ".bj" or msgs[0] == ".21" or msgs[0] == ".blackjack") and len(msgs) == 2:
                 if msgs[1] == "create":
                     Plugin.initGame(methods,info)
                 elif msgs[1] == "join":
@@ -141,11 +167,8 @@ class Plugin():
                 elif msgs[1] == "hit":
                     Plugin.hit(methods,info)
                 elif msgs[1] == "stand":
-                    methods["send"](info["address"],info["prefix"].split("!")[0]+" has chosen not to pick another card!")
-                    Plugin.next_player(methods,info)
-                elif msgs[1] == "leave":
-                    Plugin.remove(methods,info)
+                    Plugin.stand(methods,info)
                 else:
-                    methods["send"](info["address"],"invalid")
+                    methods["send"](info["address"],"invalid blackjack command")
         except Exception as e:
             print('woops blackjack plugin error: ', e)
