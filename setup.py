d097b4517cd15a91a141d55d1c434a6729d6b031
Dizzerin <46080287+Dizzerin@users.noreply.github.com>
2019-06-03 11:09:51 -0700

    Updated setup.py to read README.md (#167)
    * Started working on setup.py
    
    * Updated the long description in setup.py to read README.md

--- setup.py
+++ setup.py
@@ -7,41 +7,34 @@ if not sys.version_info[0] == 3:
     sys.exit('Sorry, python2 is not supported')
 
 project_name = 'HoneyBot'
-long_description = '''
-HoneyBot is an IRC bot written in python3,
-and distributed under MIT license.
 
-Current features include:
-- OOP architecture
-- keyword parameters
-- password security with config file [disabled for now]
-- plugins like calc, self trivia, quotes, and many more
-'''
+readme_file = open("README.md", "r", encoding="utf8")
+long_description = readme_file.read()
 
 setup(
-    name = project_name,
-    version = '5.0.0',
-    description = 'HoneyBot is a python-based IRC bot. (python3)',
-    long_description = long_description,
-    url = 'https://github.com/Abdur-rahmaanJ/honeybot',
-    download_url = 'https://github.com/Abdur-rahmaanJ/honeybot/tags',
-    author = 'Abdur-Rahmaan Janhangeer',
-    author_email = 'arj.python@gmail.com',
-    license = 'MIT',
-    include_package_data = True,
+    name=project_name,
+    version='5.0.0',
+    description='HoneyBot is a python-based IRC bot. (python3)',
+    long_description=long_description,
+    url='https://github.com/Abdur-rahmaanJ/honeybot',
+    download_url='https://github.com/Abdur-rahmaanJ/honeybot/tags',
+    author='Abdur-Rahmaan Janhangeer',
+    author_email='arj.python@gmail.com',
+    license='MIT',
+    include_package_data=True,
 
-    packages = ['monopoly_assets', 'poker_assets'],
-    package_dir = {
-        'monopoly_assets' : 'honeybot/plugins/monopoly_assets',
-        'poker_assets' : 'honeybot/plugins/poker_assets'
+    packages=['monopoly_assets', 'poker_assets'],
+    package_dir={
+        'monopoly_assets': 'honeybot/plugins/monopoly_assets',
+        'poker_assets': 'honeybot/plugins/poker_assets'
     },
-    
+
     python_requires='>=3.0',
     classifiers=[
-         'Development Status :: 5 - Production/Stable',
-         'Topic :: Utilities',
-         'License :: OSI Approved :: MIT License',
-         'Programming Language :: Python :: 3',
-         'Programming Language :: Python :: 3.7'
-      ]
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7'
+    ]
 )
