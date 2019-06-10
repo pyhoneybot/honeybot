cbc690b4db8eef437ddd7cc411f4d1460ba0e5e2
moffetma <moffetma@oregonstate.edu>
2019-06-10 10:59:17 -0700

    Change tabs to 4 spaces

--- honeybot/plugins/dadjoke.py
+++ honeybot/plugins/dadjoke.py
@@ -3,7 +3,7 @@
 [dadjoke.py]
 Dad joke plugin
 [Author]
-moffetma Oregon State University CS 
+moffetma Oregon State University CS
 [About]
 Sends a random "dad joke" from icanhazdadjoke.com
 [Commands]
@@ -11,22 +11,23 @@ Sends a random "dad joke" from icanhazdadjoke.com
 Have you heard of the band 1023MB? They haven't got a gig yet.
 """
 
-import requests 
+
+import requests
 
 class Plugin:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def dadjoke(self):
-		headers = {'Accept' : 'application/json'}
-		r = requests.get('https://icanhazdadjoke.com', headers=headers)
-		j = r.json()
-		joke = j['joke']
-		return joke
+    def dadjoke(self):
+        headers = {'Accept' : 'application/json'}
+        r = requests.get('https://icanhazdadjoke.com', headers=headers)
+        j = r.json()
+        joke = j['joke']
+        return joke
 
-	def run(self, incoming, methods, info, bot_info):
-		try:
-			if info['command'] == 'PRIVMSG' and info['args'][1] == '.dadjoke':
-				methods['send'](info['address'], Plugin.dadjoke(self))
-		except Exception as e:
-			print('woops dadjoke plugin error ', e)
+    def run(self, incoming, methods, info, bot_info):
+        try:
+            if info['command'] == 'PRIVMSG' and info['args'][1] == '.dadjoke':
+                methods['send'](info['address'], Plugin.dadjoke(self))
+        except Exception as e:
+            print('woops dadjoke plugin error', e)
