355bccc57394fadcc9a5291a3e37d0be22b9fed6
Christian Schneeweiss <christianschneeweiss@icloud.com>
2019-06-16 20:41:59 +0200

    created run.py file to run the bot and changed CONTRIBUTING.md to contain the updated procedure

--- honeybot/main.py
+++ honeybot/main.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 
 import configparser
@@ -20,7 +19,7 @@ logger = logging.getLogger('bot_core')
 
 class Bot_core(object):
     def __init__(self, password=''):
-
+        
         self.server_url = connect_config['INFO']['server_url']
         self.port = int(connect_config['INFO']['port'])
         self.name = connect_config['INFO']['name']
@@ -30,42 +29,44 @@ class Bot_core(object):
         self.autojoin_channels = self.configfile_to_list('AUTOJOIN_CHANNELS')
         self.required_modules = self.requirements()
         self.time = time.time()
-
+        
         self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.isListenOn = 1
         dom = self.server_url.split('.')
         self.domain = '.'.join(dom[-2:])
         self.sp_command = 'hbot'
         self.plugins = []
-
+    
     '''
     STRINGS
     '''
+    
     def set_nick_command(self):
         return 'NICK ' + self.name + '\r\n'
-
+    
     def present_command(self):
-        return 'USER ' + self.name + ' ' + self.name + ' ' +\
-        self.name + ' : ' + self.name + ' IRC\r\n'
-
+        return 'USER ' + self.name + ' ' + self.name + ' ' + \
+               self.name + ' : ' + self.name + ' IRC\r\n'
+    
     def identify_command(self):
         return 'msg NickServ identify ' + self.password + ' \r\n'
-
+    
     def join_channel_command(self, channel):
         return 'JOIN ' + channel + ' \r\n'
-
+    
     def specific_send_command(self, target, msg):
         return "PRIVMSG " + target + " :" + msg + "\r\n"
-
+    
     def pong_return(self, domain):
         return 'PONG :{}\r\n'.format(domain)
-
+    
     def message_info(self, s):
         def prevent_none(x):
             if x is None:
                 return ''
             else:
                 return x
+        
         try:
             prefix = ''
             trailing = []
@@ -88,15 +89,15 @@ class Bot_core(object):
             user = prefix.split('!')[0]
             # return prefix, command, args, address
             return {
-                    'prefix': prevent_none(prefix),
-                    'command': prevent_none(command),
-                    'args': ['' if e is None else e for e in args],
-                    'address': prevent_none(address),
-                    'user': prevent_none(user)
-                    }
+                'prefix': prevent_none(prefix),
+                'command': prevent_none(command),
+                'args': ['' if e is None else e for e in args],
+                'address': prevent_none(address),
+                'user': prevent_none(user)
+            }
         except Exception as e:
             logger.error(e)
-
+    
     def bot_info(self):
         return {
             'name': self.name,
@@ -106,26 +107,28 @@ class Bot_core(object):
             'time': self.time,
             'friends': self.friends
         }
+    
     '''
     MESSAGE UTIL
     '''
+    
     def send(self, msg):
         self.irc.send(bytes(msg, "UTF-8"))
-
+    
     def send_target(self, target, msg):
         self.send(self.specific_send_command(target, msg))
-
+    
     def join(self, channel):
         self.send(self.join_channel_command(channel))
-
+    
     '''
     BOT UTIL
     '''
-
+    
     def load_plugins(self, plugins_to_load):
         list_to_add = self.plugins
         logger.info('Loading plugins...')
-
+        
         to_load = []
         plugs = 'settings/{}.conf'.format(plugins_to_load)
         with open(plugs) as f:
@@ -133,50 +136,50 @@ class Bot_core(object):
             to_load = list(filter(lambda x: x != '', to_load))
         for file in to_load:
             try:
-                module = importlib.import_module('plugins.'+file)
+                module = importlib.import_module('plugins.' + file)
             except ModuleNotFoundError as e:
                 logger.warning(f"module import error, skipped' {e} in {file}")
             obj = module.Plugin
             list_to_add.append(obj)
-
+        
         self.plugins = list_to_add
         logger.info('Loaded plugins...')
-
+    
     def configfile_to_list(self, filename):
         elements = []
         with open('settings/{}.conf'.format(filename)) as f:
             elements = f.read().split('\n')
             elements = list(filter(lambda x: x != '', elements))
         return elements
-
+    
     def methods(self):
         return {
-                'send_raw': self.send,
-                'send': self.send_target,
-                'join': self.join,
-                'mem_add': self.memory_add_value,
-                'mem_rem': self.memory_remove_value,
-                'mem_fetch': self.memory_fetch_value
-                }
-
+            'send_raw': self.send,
+            'send': self.send_target,
+            'join': self.join,
+            'mem_add': self.memory_add_value,
+            'mem_rem': self.memory_remove_value,
+            'mem_fetch': self.memory_fetch_value
+        }
+    
     def run_plugins(self, listfrom, incoming):
         '''
         incoming is the unparsed string. refer to test.py
         '''
-        #if self.message_info(incoming)['args'][1][0] == ".":
-            #print("\033[0;32mReceived!\033[0;0m")
-
+        # if self.message_info(incoming)['args'][1][0] == ".":
+        # print("\033[0;32mReceived!\033[0;0m")
+        
         for plugin in listfrom:
-            #print(f"\033[0;33mTrying {plugin}\033[0;0m")
+            # print(f"\033[0;33mTrying {plugin}\033[0;0m")
             plugin.run(self, incoming, self.methods(), self.message_info(incoming), self.bot_info())
-
+    
     def requirements(self):
         reqs = []
         with open('../requirements.txt') as f:
             reqs = f.read().split('\n')
         reqs = [m.split('==')[0] for m in reqs if m]
         return reqs
-
+    
     # TODO: classify methods according to APIs and have
     # a memory API
     def memory_add_value(self, memfile, section, key, value):
@@ -184,42 +187,45 @@ class Bot_core(object):
         memory_reader[section][key] = value
         with open('memory/{}.txt'.format(memfile), 'w') as file:
             memory_reader.write(file)
-
+    
     def memory_remove_value(self, memfile, section, key):
         memory_reader.read('memory/{}.txt'.format(memfile))
         memory_reader.remove_option(section, key)
         with open('memory/{}.txt'.format(memfile), 'w') as file:
             memory_reader.write(file)
-
+    
     def memory_fetch_value(self, memfile, section, key):
         memory_reader.read('memory/{}.txt'.format(memfile))
         return memory_reader[section][key]
+    
     '''
     MESSAGE PARSING
     '''
+    
     def core_commands_parse(self, incoming):
-
+        
         '''
         PLUGINS
         '''
-
+        
         self.run_plugins(self.plugins, incoming)
-
+    
     '''
     BOT IRC FUNCTIONS
     '''
+    
     def connect(self):
-            self.irc.connect((self.server_url, self.port))
-
+        self.irc.connect((self.server_url, self.port))
+    
     def identify(self):
         self.send(self.identify_command())
-
+    
     def greet(self):
         self.send(self.set_nick_command())
         self.send(self.present_command())
         for channel in self.autojoin_channels:
             self.send(self.join_channel_command(channel))
-
+    
     def pull(self):
         while self.isListenOn:
             try:
@@ -229,7 +235,7 @@ class Bot_core(object):
                 self.stay_alive(msg)
                 self.core_commands_parse(msg)
                 logger.info(msg)
-
+                
                 if len(data) == 0:
                     try:
                         logger.critical(f'<must handle reconnection - {len(data)}==0>')
@@ -238,8 +244,7 @@ class Bot_core(object):
                         logger.info(e)
             except Exception as e:
                 logger.info(e)
-
-
+    
     # all in one for registered bot
     def registered_run(self):
         self.connect()
@@ -247,16 +252,17 @@ class Bot_core(object):
         self.greet()
         self.load_plugins('PLUGINS')
         self.pull()
-
+    
     def unregistered_run(self):
         self.connect()
         self.greet()
         self.load_plugins('PLUGINS')
         self.pull()
-
+    
     '''
     ONGOING REQUIREMENT/S
     '''
+    
     def stay_alive(self, incoming):
         if not incoming:
             logger.critical('<must handle reconnection - incoming is not True>')
@@ -267,23 +273,3 @@ class Bot_core(object):
             logger.warning(parts[1])
             self.send(self.pong_return(self.domain))
             self.send(self.pong_return(parts[1]))
-
-
-if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s %(name)s %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-    )
-
-    # logger.debug("debug message")
-    # logger.info("info message")
-    # logger.warning("warn message")
-    # logger.error("error message")
-    # logger.critical("critical message")
-    try:
-        x = Bot_core()
-        x.unregistered_run()
-    except KeyboardInterrupt:
-        print('interrupted')
-        sys.exit()
