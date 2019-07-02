47c6880abcf081fe0c0b8a3738b2f6783d6b6067
WillBurnham <burnham_will@yahoo.com>
2019-07-02 09:02:22 -0500

    New Secure Password Generator Command

--- honeybot/plugins/password_generator.py
+++ honeybot/plugins/password_generator.py
@@ -54,6 +54,44 @@ class Plugin:
         random.SystemRandom().shuffle(passwordList)
         password = ''.join(passwordList)
         return password
+    
+    def __securePasswordGenerator():
+        password = ""
+        person = ['I', 'i', 'He', 'he', 'She', 'she', 'It', 'it']
+        verb = [['am', 'is'],['run', 'runs'], ['go', 'goes'], ['drink', 'drinks'], ['eat', 'eats'], ['sleep', 'sleeps'], ['like', 'likes']]
+        prep = ['to', 'under', 'along', 'within', 'until', 'into']
+        article = ['the', 'a']
+        thing = ['bridge', 'car', 'store', 'shirt', 'computer', 'bed', 'couch']
+
+        password += person[random.randint(0,len(person) - 1)]
+
+        if password == person[0] or password == person[1]:
+            password += verb[random.randint(0,len(verb) - 1)][0]
+        else:
+            password += verb[random.randint(0,len(verb) - 1)][1]
+
+        password += prep[random.randint(0, len(prep) - 1)]
+        password += article[random.randint(0, len(article) - 1)]
+        password += thing[random.randint(0, len(thing) - 1)]
+
+        char_converter = {'i': '1', 'e': '3', 'a': '@', ' ': '-', 'o': '0'}
+        i = random.randint(0, 2)
+        new = list(password)
+
+        if i % 2 == 0:
+            for i in range(len(password)):
+                if new[i] in char_converter:
+                    new[i] = char_converter[new[i]]
+            new = ''.join(new)
+            return new
+
+        else:
+            num = round(len(password) / 2)
+            for i in range(num + 1):
+                if new[i] in char_converter:
+                    new[i] = char_converter[new[i]]
+            new = ''.join(new)
+            return new
 
     def run(self, incoming, methods, info, bot_info):
         try:
