e68b20aed063eeff44b020eefd0a5ff5ffdf9a22
Sam Deans <NomadSam@sam-deans-mbp.home>
2019-07-13 14:05:41 +0100

    Update pynews to use bs4

--- honeybot/plugins/pynews.py
+++ honeybot/plugins/pynews.py
@@ -17,29 +17,24 @@ Finds the top five news items on python.org and prints them to the IRC
 returns the top 5 python.org news articles
 """
 
-
-import requests,requests_html
+import requests
+from bs4 import BeautifulSoup
 
 class Plugin:
     def __init__(self):
         pass
-
     def run(self, incoming, methods, info, bot_info):
         try:
-            #msgs = info['args'][1:][0].split()
-
             if info['command'] == 'PRIVMSG' and info['args'][1] == '.pynews':
-                session = HTMLSession()
-                py_site = session.get('https://www.python.org')
-                # Get a list of <li> elements under the blog heading.
-                blog_links = py.html.find('div.shrubbery')[0].find('ul.menu')[0].find('li')
-                # Iterate through the list to preserve the order of the blog links
-                links = []
-                for element_li in blog_links:
-                    links.append(': '.join([element_li.text,element_li.links.pop()]))
-                # Return the links and comments separated by newlines
-                message = '\n'.join(links)
-                methods['send'](info['address'], message)
-
+                # Get python website
+                response = requests.get('https://www.python.org')
+                html = BeautifulSoup(response.text)
+                # The news section is the first <div class='shrubbery'> in the page
+                # So get all the links in that section
+                news_links = html.find('div',class_='shrubbery').find('ul', class_ = 'menu').find_all('a')
+                # Iterate through the links, send each one to the chat with its
+                # associated title (a.contents[0])
+                for a in news_links:
+                    methods['send'](info['address'],': '.join([a.contents[0],a['href']]))
         except Exception as e:
             print('woops plugin', __file__, e)
