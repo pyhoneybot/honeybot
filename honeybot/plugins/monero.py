209b444375a1d61acdf7489db13de4f48d836f0e
Arthur Eigenbrot <aeigenbrot@nso.edu>
2019-10-11 11:19:37 -0600

    Can specify any currency you want!
    Default is USD

--- honeybot/plugins/monero.py
+++ honeybot/plugins/monero.py
@@ -11,8 +11,9 @@ A. Eigenbrot
 Checks the current price for Monero via the cryptocompare.com API
 
 [Commands]
->>> .xmr
-returns current value of Monero in USD
+>>> .xmr [currency]
+returns current value of Monero in the currency specified (default is USD). Currency must be a 3-letter capital string.
+Examples: USD, EUR, BTC, GBP
 """
 
 from requests import Session
@@ -24,9 +25,15 @@ class Plugin:
 
     def run(self, incoming, methods, info, bot_info):
         try:
-            if info['command'] == 'PRIVMSG' and info['args'][1] == '.xmr':
+            msgs = info['args'][1:][0].split()
+            if info['command'] == 'PRIVMSG' and msgs[0] == '.xmr':
+                try:
+                    currency = msgs[1]
+                except:
+                    currency = 'USD'
+
                 api_url = "https://min-api.cryptocompare.com/data/price"
-                params = {'fsym': 'XMR', 'tsyms': 'USD'}
+                params = {'fsym': 'XMR', 'tsyms': currency}
 
                 session = Session()
                 headers = {'Accepts': 'application/json'}
@@ -34,7 +41,7 @@ class Plugin:
 
                 result = session.get(api_url, params=params)
                 data = json.loads(result.text)
-                methods['send'](info['address'], '{:.2f} USD'.format(data['USD']))
+                methods['send'](info['address'], '{:.2f} {}'.format(data[currency], currency))
 
         except Exception as e:
             print('woops! monero plugin error:', e)
