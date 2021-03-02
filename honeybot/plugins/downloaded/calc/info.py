7ec09bb2c6ab5c5e9c14dd050cfd81839d9fa995
Souhit Dey <suprememightyzeus@gmail.com>
2021-03-02 08:29:38 +0530

    Add info.py for calc folder

---
+++ honeybot/plugins/downloaded/calc/info.py
@@ -0,0 +1,16 @@
+
+NAME = 'abbreviation.py'
+ORIGINAL_AUTHORS = [
+    'Abdur-Rahmaan Janhangeer, pythonmembers.club'
+]
+
+ABOUT = '''
+evaluates maths expressions in the format supported by py
+'''
+
+COMMANDS = '''
+>>> .calc <maths expression>
+returns evaluated expression
+'''
+
+WEBSITE = ''
