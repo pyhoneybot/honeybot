b04c94adeb343e71ccc5cb1931c564963f3e3c8c
Kevin Bonanno <59422394+KevinBanana@users.noreply.github.com>
2021-03-03 11:08:10 -0500

    Docstring moved to info.py

---
+++ honeybot/plugins/downloaded/book_of_day/info.py
@@ -0,0 +1,15 @@
+NAME = 'book_of_day.py'
+
+ORIGINAL_AUTHORS = [
+    'Schoberg',
+    'UMD'
+]
+
+ABOUT = '''
+Using bookoftheday.org, get the title and link of the book of the day
+'''
+
+COMMANDS = '''
+>>> .bookoftheday
+https://bookoftheday.org/fleishman-is-in-trouble-taffy-brodesser-akner/
+'''
