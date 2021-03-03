342b956b7b2522fbcef083499bd20689cc3b832e
Kevin Bonanno <59422394+KevinBanana@users.noreply.github.com>
2021-03-03 11:32:50 -0500

    Add docstring for blackjack, age, and bitcoin

---
+++ honeybot/plugins/downloaded/age/info.py
@@ -0,0 +1,14 @@
+NAME = 'age.py'
+
+ORIGINAL_AUTHORS = [
+    'Justin Walker'
+]
+
+ABOUT = '''
+Given birthday it returns your age.
+'''
+
+COMMANDS = '''
+>>> .age <<day>> <<month>> <<year>>
+returns age in yr/mo/day format.
+'''
