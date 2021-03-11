73ab4a266f8ff78cb181e6583f0cfdfc5d62ad9e
Andrea-Pasini <andrypasini@hotmail.com>
2021-03-11 12:39:36 +0100

    Added 4 facts

--- honeybot/plugins/downloaded/fact/main.py
+++ honeybot/plugins/downloaded/fact/main.py
@@ -144,4 +144,15 @@ FACT_LIST = [
     + "scrub the latrine... Who screams at you for being late to work.",
     "In 1973, Mao Zedong told Henry Kissinger that China had an excess "
     + "of females and offered the U.S. 10 million Chinese woman.",
+    "Play-Doh started out as a wallpaper cleaner before the head of the", 
+    + "struggling company realized the non-toxic material made a good modeling clay for children and rebranded it.",
+    "Another term for the hashtag or pound sign is \"octothorpe\".",
+    "If you start counting at one and spell out the numbers as you go, "
+    + "you won't use the letter A until you reach 1,000.",
+    "In Spain, Mr. Clean is known as Don Limpio.",
+    "In a 2008 survey, 58% of British teens thought Sherlock Holmes was "
+    + "a real guy, while 20% thought Winston Churchill was not.",
+    "Fredric Baur invented the Pringles can. When he passed away in 2008, "
+    + "his ashes were buried in one.", 
+    
 ]
