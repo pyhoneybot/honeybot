bc1e27c970e004259fc93496bb343f1da39e9a8f
RAKESH SEAL <rakeshseal0@gmail.com>
2022-10-06 15:18:46 +0530

    plugin: add support for duckduckgo search. (#151)
    * Added support for duckduckgo search.
    
    * added pr comments fix.
    
    * added duck emoji

---
+++ src/honeybot/plugins/downloaded/duckduckgo/info.py
@@ -0,0 +1,11 @@
+NAME = "duck.py"
+ORIGINAL_AUTHORS = ["Rakesh Seal"]
+
+ABOUT = """
+Returns a abstract of the search query from a duckduckgo Search. Return empty if no abstract found.
+"""
+
+COMMANDS = """
+>>> .duck <query>
+returns abstrack from duckduckgo search
+"""
