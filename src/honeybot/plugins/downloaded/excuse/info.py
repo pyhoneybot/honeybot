3366e77534becdf0ed7e32f000994e8f3bdfb19e
RAKESH SEAL <rakeshseal0@gmail.com>
2022-10-06 15:11:31 +0530

    Added new plugin for random excuses.

---
+++ src/honeybot/plugins/downloaded/excuse/info.py
@@ -0,0 +1,11 @@
+NAME = "excuse.py"
+ORIGINAL_AUTHORS = ["Rakesh Seal"]
+
+ABOUT = """
+Returns a random excuse for you.
+"""
+
+COMMANDS = """
+>>> .excuse
+returns a random image url.
+"""
---
+++ src/honeybot/plugins/downloaded/excuse/main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+[excuse.py]
+Random excuse Plugin
+
+[Author]
+Rakesh Seal
+
+[About]
+Returns a random excuse for you.
+[Website]
+
+
+[Commands]
+>>> .excuse
+Returns a random excuse for you.
+"""
+
+import requests
+
+
+class Plugin:
+    def __init__(self):
+        self.url = "https://excuser.herokuapp.com/v1/excuse"
+
+    def run(self, incoming, methods, info, bot_info):
+        try:
+            msgs = info["args"][1:][0].split()
+            if info['command'] == 'PRIVMSG' and msgs[0] == '.excuse':
+                    excuse = requests.get(self.url).json()[0]['excuse']
+                    methods['send'](info['address'], excuse)
+        except Exception as e:
+            print("No excuse for you!", e)
