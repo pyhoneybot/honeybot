28ce68a74f3d7de5f558eff2819a5dcc777218e5
RAKESH SEAL <rakeshseal0@gmail.com>
2022-10-06 15:46:20 +0530

    reformatted with black.

--- src/honeybot/plugins/downloaded/duckduckgo/main.py
+++ src/honeybot/plugins/downloaded/duckduckgo/main.py
@@ -22,16 +22,18 @@ import requests
 class Plugin:
     def __init__(self):
         pass
-    
+
     def __gen_url_from_query(self, query):
         return "https://duckduckgo.com/?q=" + query + "&format=json&pretty=1"
 
     def run(self, incoming, methods, info, bot_info):
         try:
             msgs = info["args"][1:][0].split()
-            if info['command'] == 'PRIVMSG' and msgs[0] == '.duck':
-                    query = ' '.join(msg for msg in msgs[1:])
-                    duck_abstract = requests.get(self.__gen_url_from_query(query)).json()["Abstract"] 
-                    methods['send'](info['address'], duck_abstract)
+            if info["command"] == "PRIVMSG" and msgs[0] == ".duck":
+                query = " ".join(msg for msg in msgs[1:])
+                duck_abstract = requests.get(self.__gen_url_from_query(query)).json()[
+                    "Abstract"
+                ]
+                methods["send"](info["address"], duck_abstract)
         except Exception as e:
             print("quack quack!", e)
