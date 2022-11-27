af0e74bf960083ca3e528ee3c541d51b60623357
deadex-ng <thisisfumba@gmail.com>
2022-11-27 16:19:20 +0200

    added emoji plugin

---
+++ src/honeybot/plugins/downloaded/emoji/info.py
@@ -0,0 +1,12 @@
+NAME = "emoji.py"
+
+ORIGINAL_AUTHORS = ["Fumbani Banda"]
+
+ABOUT = """
+Given an emoji it returns meaning.
+"""
+
+COMMANDS = """
+>>> .emoji <<emoji>>
+returns emoji meaning.
+"""
