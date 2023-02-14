d3760388d9cad8456fd9cb1c91c953feda2c3945
Paulo Ferraz <paulosgf.pf@gmail.com>
2023-02-14 16:06:03 -0300

    pre-commit master CI validation

---
+++ src/honeybot/teste.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import time
+import math
+import os
+
+def main():
+    print(time.strftime('%Y.%m.%d'))
+    print(os.getcwd())
+    print(math.sqrt(4))
+
+if __name__ == '__main__':
+    main()
