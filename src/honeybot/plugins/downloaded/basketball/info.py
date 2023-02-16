10612a6cfe227fdf0da3ca181d1015228373f11c
kefthymic <74056501+kefthymic@users.noreply.github.com>
2023-02-16 13:35:53 +0200

    Adding the new plugin: basketball (#187)
    * Adding the new plugin: basketball
    
    * Editing the fun.rst
    
    * add the basketball plugin in README.md
    
    * Update README.md
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * format README and main.py
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

---
+++ src/honeybot/plugins/downloaded/basketball/info.py
@@ -0,0 +1,15 @@
+NAME = "basketball.py"
+
+ORIGINAL_AUTHORS = ["Konstantinos Efthymiadis"]
+
+ABOUT = """
+Given the event and year as parameters, the plugin will return the countries that won the medal that year
+Given the event and country as parameters, the plugin will return the number of medals this country has won
+"""
+
+COMMANDS = """
+>>> .basketball <<event>> <<year/country>>
+returns a string with all the information
+>>> .basketball <<event>> <<all>>
+returns a table with all the medals of all the years
+"""
